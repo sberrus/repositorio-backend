03d6f91aa2ce0446741a17d9c419e2cb7587b6ab
samdev <samuelberrus@gmail.com>
2022-07-06 13:00:08 +0200

    Agrega funcionalidad de enviar emails
    Se ha agregado funcionalidad de enviar email reciviendo los datos del
    formulario.

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
+import { sendEmail } from "../utils/emailSender";
 
 export const sendContactEmail = async (request: Request, response: Response) => {
+	const { from, subject, message } = request.body;
 	// send email
+	try {
+		await sendEmail(from, subject, message);
+	} catch (error) {}
 
 	console.log("Correo enviado con exito");
 	response.json({
